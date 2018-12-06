fc8e08c5a9841aee00fd7547b382e60cad925eef
Hung Le <skvn.clew@gmail.com>
2018-12-06 18:10:25 +0700

    add store - shouldShowFormReducer - wordReducer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,49 +9,8 @@ import { Word } from './types';
 
 export class AppComponent {
 
-  filterStatus = 'SHOW_ALL';
-  words: Word[] = [  // thêm Word[] để quét theo interface Word sai syntax thì sẽ báo
-    { en: 'One', vn: 'Mot', isMemorized: true, _id: 'abcd1' },
-    { en: 'Two', vn: 'Hai', isMemorized: false, _id: 'abcd2' },
-    { en: 'Three', vn: 'Ba', isMemorized: false, _id: 'abcd3' },
-    { en: 'Four', vn: 'Bon', isMemorized: true, _id: 'abcd4' }
-  ];
-
-  memorizedStyle = {
-    fontWeight: 500,
-    color: 'green'
-  }
-
-  forgotStyle = {
-    fontWeight: 900,
-    color: 'red'
-  }
 
 
-  onRemoveWord (_id: string) {
-    const index = this.words.findIndex(word => word._id === _id)
-    this.words.splice(index,1) // xóa từ vi trí index di 1 đơn vị
-  }
 
-  onToggleWord(_id: string) {
-    const word = this.words.find(word => word._id === _id);
-    word.isMemorized = !word.isMemorized;
-  }
-  
-  onAddWord(word: Word) {
-    this.words.unshift(word);
-  }
-
-  getFilteredWord() {
-    return this.words.filter(word => {
-      if (this.filterStatus === 'SHOW_ALL') return true;
-      if (this.filterStatus === 'SHOW_FORGOT') return !word.isMemorized;
-      return word.isMemorized;
-    });
-  }
-
-  onChangeFilterStatus(newStatus: string) {
-    this.filterStatus = newStatus;
-  }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { FormsModule } from "@angular/forms"
 
 
 import { AppComponent } from './app.component';
-import { WordComponent } from './word.component';
-import { WordFormComponent } from './word-form.component';
-import { WordFilterComponent } from './word-filter.component';
+import { shouldShowFormReducer } from './ngrx/shouldShowFormReducer';
+import { wordsReducer } from './ngrx/wordsReducer';
+
+
 import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordComponent,
-    WordFormComponent,
-    WordFilterComponent
+    wordsReducer,
+    shouldShowFormReducer
+   
   ],
   imports: [
     BrowserModule,
